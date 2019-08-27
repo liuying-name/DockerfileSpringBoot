ed2048fea73bcb3c0ec65c1991f8688655208048
123610420@qq.com <123610420@qq.com>
2019-08-28 00:56:48 +0800

    修改参数

--- src/main/java/com/liu/spring_boot/App.java
+++ src/main/java/com/liu/spring_boot/App.java
@@ -6,7 +6,7 @@ package com.liu.spring_boot;
  */
 public class App {
 	public static void main(String[] args) {
-
+		System.out.println("Hello World!aaaaaaaaaaaaaaaa");
 		System.out.println("Hello World!4");
 		
 		System.out.println("Hello World!5");
